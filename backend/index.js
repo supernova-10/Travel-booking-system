f12fbb025a94e850f0bcc325dc93818825fc0000
the-refrigerator <65685646+the-refrigerator@users.noreply.github.com>
2023-08-27 19:09:18 +0300

    pointed `npm start` script to index.js instead of server.js

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,25 @@
-// connect db and start server 
-import app from './server.js'
-import mongodb from'mongodb'
-import dotenv from 'dotenv'
-import travelDAO from './dao/travelDAO.js'
-dotenv.config()
-const MongoClient = mongodb.MongoClient
+// connect db and start server
+import app from "./server.js";
+import mongodb from "mongodb";
+import dotenv from "dotenv";
+import travelDAO from "./dao/travelDAO.js";
+dotenv.config();
+const MongoClient = mongodb.MongoClient;
 
-const port=process.env.PORT || 8000 // orignally its 5000 but if that cannot be accessed use this instead
-MongoClient.connect(
-    process.env.travel_DB_URI,
-    {
-      poolSize: 50,
-      wtimeout: 2500,
-      useNewUrlParse: true }
-    )
-    .catch((error) => console.log(error.message))
-    .then(async client => {
-        await travelDAO.injectDB(client)
-      app.listen(port, () => {
-        console.log(`listening on port ${port}`)
-      })
-    })
+const port = process.env.PORT || 8000; // orignally its 5000 but if that cannot be accessed use this instead
+MongoClient.connect(process.env.travel_DB_URI, {
+	poolSize: 50,
+	wtimeout: 2500,
+	useNewUrlParse: true,
+})
+	.catch((error) => console.log(error.message))
+	.then(async (client) => {
+		await travelDAO.injectDB(client);
+		app.listen(port, () => {
+			console.log(`listening on http://127.0.0.1:${port}`);
+		});
+	});
 
-    //connect database
-    // start webserver
-//pass db uri 
+//connect database
+// start webserver
+//pass db uri
--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,20 @@
-// configure an express server 
+// configure an express server
 // send and receive json by specific routes
 
-import express from 'express'
-import cors from 'cors'
-import travel from "./api/travel.route.js"
+import express from "express";
+import cors from "cors";
+import travel from "./api/travel.route.js";
 
-const app = express() // used to make the server 
-app.use(cors()) //middleware using cors module to allow cross origin requests
-app.use(express.json()) //middleware to parse json data server can accept json in the body of the request sent by git 
+const app = express(); // used to make the server
+app.use(cors()); //middleware using cors module to allow cross origin requests
+app.use(express.json()); //middleware to parse json data server can accept json in the body of the request sent by git
 
 //specify some initial routes
-app.use("/api/v1/travel", travel)
-// if smn tried to go to a different route, that doesnt exit 
-app.use("*", (req, res) => res.status(404).json({ error: "not found" }))
+app.use("/api/v1/travel", travel);
+// if smn tried to go to a different route, that doesnt exit
+app.use("*", (req, res) => res.status(404).json({ error: "not found" }));
 
-// export app as a module 
+// export app as a module
 
 export default app;
 // import this module in the file that accesses the database, run this file to get the server running
